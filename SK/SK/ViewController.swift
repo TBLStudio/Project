d54cb426fe83544cb99ba70451df1efd660f1c53
TBLStudio <iosdev.tbl@gmail.com>
2016-05-03 13:48:37 +0700

    - Add Side Menu - Add PureLayout third party lib

---
+++ SK/SK/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SK
+//
+//  Created by Thái Bô Lão on 5/3/16.
+//  Copyright © 2016 Thái Bô Lão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Setup navigation bar
+        title = "Main ViewController"
+        
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
